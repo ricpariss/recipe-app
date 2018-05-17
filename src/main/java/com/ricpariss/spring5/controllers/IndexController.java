a5001ecc9a259b5d9c24a30930646496baed295a
Ricardo <Ricardo@DESKTOP-MI6Q5J9.attlocal.net>
2018-05-17 11:36:33 -0400

    version 0.9

---
+++ src/main/java/com/ricpariss/spring5/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.ricpariss.spring5.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"","/","/index"})
+	public String getIndexPage() {
+		return "index";
+	}
+
+}
